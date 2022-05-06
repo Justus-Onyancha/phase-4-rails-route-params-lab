9b0f919e93b49a7d4e68d92498b4938936e892a8
Justus-Onyancha <justus.onyancha@moringaschool.com>
2022-05-06 03:50:02 +0300

    completed lab/assignement

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,18 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+
+    students = Student.find_by(id: params[:id])
+    render json: students
+  end
+
+  def grades
+
+  end
+
+  def highest_grade
+  end
+
+
 end
